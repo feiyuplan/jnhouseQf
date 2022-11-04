ba59ec5a502b6b528e0bd899a0cda33dc54f02f6
feiyu <315061897@qq.com>
2022-11-04 11:45:02 +0800

    引用env封装

--- src/Kernel/Until/EnvCache.php
+++ src/Kernel/Until/EnvCache.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace Feiyuplan\Jnhouse\Kernel\Until;
-use MillionMile\GetEnv\Env as BaseEnv;
+use Feiyuplan\Jnhouse\Kernel\Until\Env;
 class EnvCache
 {
     public static function init(){
         try {
-            BaseEnv::loadFile('.env');
+            Env::loadFile('.env');
         } catch (Exception $e) {
             echo $e->getMessage();
             exit;
         }
     }
     public static function get($name,$default=null){
-        return BaseEnv::get($name,$default);
+        return Env::get($name,$default);
     }
 }
